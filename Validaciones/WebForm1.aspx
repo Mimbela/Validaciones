8556ef13defa296dcb846b5f5086bdde1f315650
senin8 <malizziaaries@gmail.com>
2018-05-31 13:16:23 +0200

    Add project files.

---
+++ Validaciones/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Validaciones.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+   <script type="text/javascript">
+        function leerOcultos() {
+            alert(document.getElementById("OcultoNombre").getAttribute("value")
+                + "\n" + document.getElementById("OcultoPerfil").getAttribute("value"));
+
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+       Nombre: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br />
+        Apellido: <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="NET" />
+       <asp:CheckBox ID="CheckBox2" runat="server" Text="PHP" />
+        <asp:CheckBox ID="CheckBox3" runat="server" Text="JAVA" />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="ACEPTAR" OnClick="Button1_Click" OnClientClick="leerOcultos()" />
+        <br />
+        <span id="nombreCompleto" runat="server"></span>
+        <br />
+        <span id="perfilCompleto" runat="server"></span>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Validaciones/WebForm1.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Validaciones
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string concatenarNombre()
+        {
+            string nombreCompleto;
+            nombreCompleto = TextBox1.Text + " " + TextBox2.Text;
+            return nombreCompleto.ToUpper();
+        }
+
+        private string concatenarPerfil()
+        {
+            string perfilCompleto="";
+            if (CheckBox1.Checked)
+            {
+                perfilCompleto += CheckBox1.Text + " ";
+            }
+            if (CheckBox2.Checked)
+            {
+                perfilCompleto +=CheckBox2.Text + " ";
+            }
+            if (CheckBox3.Checked)
+            {
+                perfilCompleto += CheckBox3.Text + "";
+            }
+            return perfilCompleto;
+        }
+
+        private void crearCampoOcultoNombre (string nombre)
+        {
+            ClientScriptManager campo = Page.ClientScript;
+            campo.RegisterHiddenField("OcultoNombre", nombre);
+        }
+
+        private void crearCampoOcultoPerfil (string perfil)
+        {
+            ClientScriptManager campo = Page.ClientScript;
+            campo.RegisterHiddenField("OcultoPerfil", perfil);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            nombreCompleto.InnerHtml = "<br><b>" + concatenarNombre() + "</b>";
+            perfilCompleto.InnerHtml = "<b>" + concatenarPerfil() + "</b>";
+
+            crearCampoOcultoNombre(concatenarNombre());
+            crearCampoOcultoPerfil(concatenarPerfil());
+
+            Button1.Text = "oCULTOS";
+      
+        }
+    }
+}
